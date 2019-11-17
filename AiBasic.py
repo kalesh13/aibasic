d3a80cc144f855f9ca2efda207db4f158fbff4d0
Kalesh Kaladharan <kalesh.13@gmail.com>
2019-11-17 23:35:50 +0530

    Updated to CookieCutter template

--- AiBasic.py
+++
@@ -1,9 +0,0 @@
-from matplotlib import pyplot as plt
-
-x = [1, 2, 3, 4, 5]
-
-y = [2, 4, 1, 9, 3]
-
-plt.bar(x, y)
-
-plt.show()
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Ai/Machine Learning/Python course project',
+    author='Kalesh Kaladharan',
+    license='MIT',
+)
